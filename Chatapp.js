304131758b22aa5ffe2313ea35f5222c3486754a
Soumya1002 <soumyasenthil1002@gmail.com>
2024-02-04 12:07:39 +0530

    Task2: Creating Signup Screen Backend Apis

---
+++ Chatapp.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+// app.use(express.static('public'))
+require('dotenv').config()
+const path = require('path')
+
+const fs = require('fs')
+
+const User = require('./Models/user-model')
+
+const sequelize = require('./util/database')
+const cors = require('cors')
+
+const app = express()
+
+app.use(cors())
+
+app.use(bodyParser.json({extended:false}))
+
+const signupRoute = require('./Routes/signup-route')
+
+app.use(signupRoute)
+
+app.use((req, res) => {
+    res.sendFile(path.join(__dirname, `public/${req.url}`))
+})
+
+
+sequelize.sync()
+.then(() => {
+    app.listen(4000)
+}) 
+.catch(err => {
+    console.log(err)
+})
