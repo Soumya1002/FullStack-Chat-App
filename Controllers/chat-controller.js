b684e054cf290e29c274588d2c56e9880b0818f3
Soumya1002 <soumyasenthil1002@gmail.com>
2024-02-07 09:53:11 +0530

    ChatApp backend created ...setInterval is 1 second refreshes screen automatically

--- Controllers/chat-controller.js
+++ Controllers/chat-controller.js
@@ -1,9 +1,17 @@
-
 const User = require('../Models/user-model')
 const Chat = require('../Models/chat-model')
 
-const messageReceived = async (req, res, next) => {
+const getAllChats = async (req, res, next) => {
+    try{
+            const allChats = await Chat.findAll()
+            res.status(200).json(allChats)
+    } catch(err){
+        console.log('Error in getAllChats function>>>>>>>>>>>>>>>>>>>>>>', err)
+        res.status(500).json(err, {message: 'Internal server error'})
+    }
+}
 
+const messageReceived = async (req, res, next) => {
     try{
         console.log(req.body.message)
         const message = req.body.message
@@ -11,12 +19,15 @@ const messageReceived = async (req, res, next) => {
 
         const data = await Chat.create({chat: message, GroupId: groupId, UserId: req.user.id})
 
+        res.status(200).json({message: 'successfully saved'})
+
     } catch(err){
-        console.log(err)
+        console.log('error in messageReceived', err)
+        res.status(500).json({message: 'Internal server error 500'})
     }
 
 }
-
 module.exports = {
-    messageReceived
+    messageReceived,
+    getAllChats
 }
---
+++ public/Home/home.js
@@ -0,0 +1,82 @@
+try{
+} catch(err){
+
+}
+
+window.setInterval(async () => {
+    try{
+        const token = localStorage.getItem('GCtoken')
+
+        const chatDisplayDiv = document.getElementById('chatwindow')
+
+
+        const response = await axios.get('http://localhost:4000/chat/getallchats', {
+            headers: {'authorization': token}
+        })
+
+        if(response.data.length === 0){
+            chatDisplayDiv.innerHTML = '<p class="card-text">No messages yet</p>'
+        } else{
+            chatDisplayDiv.innerHTML = ''
+            showMessages(response.data, chatDisplayDiv)
+        }
+
+    } catch(err){
+        console.log(err)
+    }
+}, 10000)
+
+window.addEventListener('DOMContentLoaded', async () => {
+    try{
+            const token = localStorage.getItem('GCtoken')
+            const chatDisplayDiv = document.getElementById('chatwindow')
+            chatDisplayDiv.innerHTML = ''
+            const response = await axios.get('http://localhost:4000/chat/getallchats', {
+                headers: {'authorization': token}
+            })
+            if(response.data.length === 0){
+                chatDisplayDiv.innerHTML = '<p class="card-text">No messages yet</p>'
+            } else{
+                showMessages(response.data, chatDisplayDiv)
+            }
+
+    } catch(err){
+        console.log(err)
+    }
+})
+
+function showMessages(data, chatDisplayDiv){
+
+    data.forEach(element => {
+        const p = document.createElement('p')
+        p.className = 'cart-text'
+        p.innerText = element.chat
+        chatDisplayDiv.appendChild(p)
+      })
+}
+
+async function logOut(e){
+    e.preventDefault()
+    localStorage.removeItem('GCtoken')
+    window.location.href = '../Login/login.html'
+}
+async function sendMessage(e){
+    e.preventDefault()
+    try {
+        const message = document.getElementById('messageinputbox').value
+        const token = localStorage.getItem('GCtoken')
+        document.getElementById('messageinputbox').value = ''
+        const messageObj = {
+            message
+        }
+        if(message != '' || message != null){
+            console.log('before emptying')
+            await axios.post('http://localhost:4000/chat/message', messageObj, {
+                headers: {'authorization': token}
+            })
+        }
+
+    } catch(err){
+        console.log(err)
+    }
+}
